eb52c1c6faacf15291370e75fe7e32d50f997b2d
unknown <bouzire.noureddine1@gmail.com>
2025-01-27 23:43:11 +0100

    outputing the values in a table

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,28 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { EventEmitter, Output } from '@angular/core';
+import type { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+  entredInitialInvestment = '0';
+  entredAnnualInterest = '0';
+  entredExpectedReturn = '5';
+  entredDuration = '10';
+  onSubmit(){
+    this.calculate.emit({
+      initialInvestment: +this.entredInitialInvestment,
+      annualInvestment: +this.entredAnnualInterest,
+      expectedReturn: +this.entredExpectedReturn,
+      duration: +this.entredDuration
+    });
+}
 }
