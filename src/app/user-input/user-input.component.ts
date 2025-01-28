393beb26245c465cf1d63f06659a0571d6365ca1
unknown <bouzire.noureddine1@gmail.com>
2025-01-28 17:00:56 +0100

    I add the investmentService to the project

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,8 +1,8 @@
 import { NgClass, NgFor } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { output } from '@angular/core';
 import type { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -12,18 +12,23 @@ import type { InvestmentInput } from '../investment-input.model';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
- calculate = output<InvestmentInput>();
+
   entredInitialInvestment = signal('0');
   entredAnnualInterest = signal('0');
   entredExpectedReturn = signal('0');
   entredDuration = signal('0');
+
+  constructor(private investmentService: InvestmentService ) {}
+
   onSubmit(){
-    this.calculate.emit({
-      initialInvestment: +this.entredInitialInvestment(),
-      annualInvestment: +this.entredAnnualInterest(),
-      expectedReturn: +this.entredExpectedReturn(),
-      duration: +this.entredDuration()
-    });
+    this.investmentService.calculateInvestmentResults(
+      {
+        initialInvestment: +this.entredInitialInvestment(),
+        duration: +this.entredDuration(),
+        expectedReturn: +this.entredExpectedReturn(),
+        annualInvestment: +this.entredAnnualInterest()
+      }
+    );
     this.entredInitialInvestment.set('0');
     this.entredAnnualInterest.set('0');
     this.entredExpectedReturn.set('0');
